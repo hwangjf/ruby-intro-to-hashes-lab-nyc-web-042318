4f2877841e2e908b2a8adad6a6642892e6e1f70b
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-18 20:37:19 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,16 @@ def actor
 end
 
 def monopoly
-	monopoly = {:railroads => {}}
+	monopoly = {:railroads => {
+	  pieces: 4,
+	  rent_in_dollars: {
+	    one_piece_owned: 25,
+	    two_pieces_owned: 50,
+	    three_pieces_owned: 75,
+	    four_pieces_owned: 100
+	    }
+	  }
+	}
 end
 
 def monopoly_with_second_tier
